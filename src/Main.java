8a16c91d5c29296a1c67b897500571c1d99e2de7
g.flo <giurgiu.florin@hotmail.com>
2023-03-06 19:56:51 +0200

    added a new print line

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello " + "world!!!!");
+        System.out.println("Hello " + "world!!!!");
         Random random = new Random();
     }
 }
